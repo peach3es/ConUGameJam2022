db5b173203453498c1316756b7c218dffe5a3d03
peaches <peizhe2001@hotmail.com>
2022-10-15 17:13:41 -0400

    Created 2 background level

--- GameJam2022(EmptyMind)/Assets/Scripts/Environment/ParallaxBackground.cs
+++ GameJam2022(EmptyMind)/Assets/Scripts/Environment/ParallaxBackground.cs
@@ -4,21 +4,30 @@ using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
-    [SerializeField] private float parallaxEffectMultiplier;
-
-    private Transform cameraTransform; 
-    private Vector3 lastCameraPosition;
+    public float parallaxEffectX;
+    public float parallaxEffectY;
+    public GameObject cam;
+    private float length, startPos;
 
 
     private void Start() {
-        cameraTransform = Camera.main.transform;
-        lastCameraPosition = cameraTransform.position;
+        startPos = transform.position.x;
+        startPos = transform.position.y;
+
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     private void Update() {
+        float temp = (cam.transform.position.x * (1 - parallaxEffectX));
+        float distx = (cam.transform.position.x * parallaxEffectX);
+        float disty = (cam.transform.position.y * parallaxEffectY);
 
-        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
-        transform.position += deltaMovement * parallaxEffectMultiplier;
-        lastCameraPosition = cameraTransform.position;
+        transform.position = new Vector3(startPos + distx, transform.position.y, transform.position.z);
+
+        if (temp > startPos + length) {
+            startPos += length;
+        } else if (temp < startPos - length) {
+            startPos -= length;
+        }
     }
 }
