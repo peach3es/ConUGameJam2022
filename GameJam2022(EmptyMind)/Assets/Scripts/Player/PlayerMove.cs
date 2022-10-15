6541601420f2d2430dc049d0308ece6359939246
Adamo O <adamo.orsini12@gmail.com>
2022-10-15 02:05:12 -0400

    Lots of useless asset changes

--- GameJam2022(EmptyMind)/Assets/Scripts/Player/PlayerMove.cs
+++ GameJam2022(EmptyMind)/Assets/Scripts/Player/PlayerMove.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
     // Character controller attributes
     public CharacterController2D controller;
