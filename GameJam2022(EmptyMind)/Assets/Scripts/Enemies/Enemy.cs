59f52a79a7cb205c234a1196599a8e8f0331d94a
Adamo Orsini <adamo.orsini01@gmail.com>
2022-10-16 14:04:23 -0400

    Added flash effect on enemy hit

--- GameJam2022(EmptyMind)/Assets/Scripts/Enemies/Enemy.cs
+++ GameJam2022(EmptyMind)/Assets/Scripts/Enemies/Enemy.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public Color flashColor = Color.red;
+
     public int health = 100;
 
     public GameObject deathEffect;
 
+    public FlashEffect flashEffect;
+
     public void TakeDamage(int damage)
     {
+        flashEffect.Flash(flashColor);
+        
         health -= damage;
 
         if (health <= 0)
