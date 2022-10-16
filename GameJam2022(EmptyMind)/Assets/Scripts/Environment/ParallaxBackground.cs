5be55bc441ec351ac68fb159a252f6902366d7b4
peaches <peizhe2001@hotmail.com>
2022-10-16 04:31:03 -0400

    fix some merge issues

--- GameJam2022(EmptyMind)/Assets/Scripts/Environment/ParallaxBackground.cs
+++ GameJam2022(EmptyMind)/Assets/Scripts/Environment/ParallaxBackground.cs
@@ -6,7 +6,7 @@ public class ParallaxBackground : MonoBehaviour
 {
     public float parallaxEffectX;
     public float parallaxEffectY;
-    public GameObject cam;
+    //public GameObject cam;
     private float length, startPos;
 
 
@@ -18,9 +18,9 @@ public class ParallaxBackground : MonoBehaviour
     }
 
     private void Update() {
-        float temp = (cam.transform.position.x * (1 - parallaxEffectX));
-        float distx = (cam.transform.position.x * parallaxEffectX);
-        float disty = (cam.transform.position.y * parallaxEffectY);
+        float temp = (Camera.main.transform.position.x * (1 - parallaxEffectX));
+        float distx = (Camera.main.transform.position.x * parallaxEffectX);
+        float disty = (Camera.main.transform.position.y * parallaxEffectY);
 
         transform.position = new Vector3(startPos + distx, transform.position.y, transform.position.z);
 
